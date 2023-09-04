dd0b033754bb59e675630831378506602339b5b0
Anton Karpov <slam.caponezed@mail.ru>
2023-09-04 22:38:23 +0300

    Added a new button, refactored some js code, sendButton icon changing animation now works with custom input too

--- vk-snippet-widget.js
+++ vk-snippet-widget.js
@@ -1,85 +1,119 @@
 let vkWidget = createVkSnippetWidget();
-let input = document.querySelector(".vk-snippet-widget__input");
-let vkTextBox;
-let sendButton;
+
 function createVkSnippetWidget() {
   let vkSnippetWidget = document.createElement("div");
   vkSnippetWidget.classList.add("vk-snippet-widget");
 
-  let inputContainer = createNode({ nodeType: "div" });
-  let buttonContainer = createNode({ nodeType: "div" });
+  let vkTextBox;
+  let sendButton;
 
-  let input = createNode({ nodeType: "input" });
+  let voiceButtonIconClass = `im-send-btn_audio`;
+  let sendButtonIconClass = `im-send-btn_send`;
 
-  let clearButton = createNode({
-    nodeType: "button",
-    name: "❌",
-    sendSnippet: false,
-  });
-  let questionButton = createNode({
-    nodeType: "button",
-    name: "❓",
-    messageToSend:
-      "Нужно ли дыроколить, скреплять скрепкой или степлером? (Бесплатно)<br>Нужен ли скоросшиватель? (15₽ шт.)",
-  });
-  let doneButton = createNode({
-    nodeType: "button",
-    name: "👍",
-    messageToSend: "Всё готово, можешь приходить в комнату 5403 ╰(*°▽°*)╯",
-  });
+  let inputContainer = createNode({ tagType: "div", sendSnippet: false });
+  let buttonContainer = createNode({ tagType: "div", sendSnippet: false });
+
+  let input = createNode({ tagType: "input" });
+  input.placeholder = "Стоимость печати (₽)";
+
   let moneyButton = createNode({
-    nodeType: "button",
+    tagType: "button",
     name: "💸",
     sendSnippet: false,
     sendNonEmptyOnClick: true,
   });
+  let clearButton = createNode({
+    tagType: "button",
+    name: "❌",
+    sendSnippet: false,
+  });
+  let questionButton = createNode({
+    tagType: "button",
+    name: "❓",
+    messageToSend:
+      "Нужно ли: <br/> - дыроколить 🕳️ <br/> - скреплять скрепкой 📎 или степлером 〰 <br /> <br /> Нужен ли скоросшиватель? 🗂️ (20₽ шт.)",
+  });
+  let iPrintButton = createNode({
+    tagType: "button",
+    name: "🖨️",
+    messageToSend:
+      "Печатаю 🖨️ (ˉ﹃ˉ) <br/> - цвет 🎨 <br/> - чб ⚫ <br/> - скрепки 📎 <br/> - скоросшиватели 🗂️ <br/> - файлики 📄) <br/> ",
+  });
+  let doneButton = createNode({
+    tagType: "button",
+    name: "👍",
+    messageToSend:
+      "Всё готово ✅ <br/> Можешь приходить в комнату 5403 ╰(*°▽°*)╯",
+  });
 
-  buttonContainer.append(clearButton);
-  buttonContainer.append(questionButton);
-  buttonContainer.append(doneButton);
-
-  inputContainer.append(input);
-
-  inputContainer.append(moneyButton);
   vkSnippetWidget.append(inputContainer);
   vkSnippetWidget.append(buttonContainer);
 
+  inputContainer.append(input);
+  inputContainer.append(moneyButton);
+
+  buttonContainer.append(clearButton);
+  buttonContainer.append(questionButton);
+  buttonContainer.append(iPrintButton);
+  buttonContainer.append(doneButton);
+
   function createNode({
-    nodeType = "div",
+    tagType = "div",
     name = "",
     messageToSend = "",
     sendSnippet = true,
     sendNonEmptyOnClick = false,
   }) {
     try {
-      let node = document.createElement(nodeType);
-      if (node.tagName == "INPUT") {
-        node.type = "number";
-        node.placeholder = "Стоимость печати (₽)";
-
-        node.addEventListener("input", handleInput);
-        node.addEventListener("keydown", sendMessageOnKeyDown);
-      }
-
+      let node = document.createElement(tagType);
       node.textContent = name;
       let cssNodeClass = node.tagName.toLowerCase();
       node.classList.add(`vk-snippet-widget__${cssNodeClass}`);
 
-      if (node.tagName == "BUTTON")
-        node.addEventListener("click", (event) => {
-          vkTextBox.click();
-          vkTextBox.innerHTML = messageToSend;
-          if (sendSnippet) sendButton.click();
-          if (sendNonEmptyOnClick) {
-            if (!input.value) return;
-            vkTextBox.innerHTML = `${input.value}₽ на 89201006977 ( Тинькофф, сбербанк ) или наличными.`;
-            sendButton.click();
-            setTimeout(() => (vkTextBox.textContent = ""), 0);
-          }
-          input.value = "";
-        });
+      switch (true) {
+        case node.tagName == "INPUT": {
+          node.type = "number";
+
+          node.addEventListener("input", handleInput);
+          node.addEventListener("keydown", sendMessageOnKeyDown);
+          break;
+        }
+        case node.tagName == "BUTTON": {
+          node.addEventListener("click", (event) => {
+            vkTextBox.click();
+            vkTextBox.innerHTML = messageToSend;
+            if (sendSnippet) sendButton.click();
+            if (sendNonEmptyOnClick) {
+              if (!input.value) return;
+              vkTextBox.innerHTML = `${input.value}₽ на 89201006977 ( Тинькофф, сбербанк ) или наличными.`;
+              sendButton.click();
+              setTimeout(() => (vkTextBox.textContent = ""), 0);
+            }
+            input.value = "";
+          });
+          break;
+        }
+      }
 
       return node;
+    } catch (err) {
+      return err;
+    }
+  }
+  function addVkWidget(e) {
+    try {
+      let vkRightMenu = document.querySelector(
+        "#content > div > div.im-right-menu > div.page_block.ui_rmenu._im_right_menu.ui_rmenu_pr"
+      );
+      vkTextBox = document.querySelector("div[role='textbox']");
+      sendButton = document.querySelector(
+        "#content > div > div > div.im-page--history.page_block._im_page_history > div.im-page-history-w > div.im-page--chat-input._im_chat_input_w > div.im-chat-input.clear_fix._im_chat_input_parent > div.im-chat-input--textarea.fl_l._im_text_input._emoji_field_wrap._voice_field_wrap > div.im-chat-input--txt-wrap._im_text_wrap > button"
+      );
+      sendButton.addEventListener("click", () => {
+        if (input.value) input.value = "";
+      });
+      if (!vkRightMenu) return;
+      vkRightMenu.append(vkWidget);
     } catch (err) {
       return err;
     }
@@ -90,7 +124,15 @@ function createVkSnippetWidget() {
     let targetValue = event.target.value;
     vkTextBox.click();
     vkTextBox.innerHTML = `${targetValue}₽ на 89201006977 ( Тинькофф, сбербанк ) или наличными.`;
+
     if (!targetValue) vkTextBox.textContent = "";
+    if (vkTextBox.innerHTML) {
+      sendButton.classList.remove(voiceButtonIconClass);
+      sendButton.classList.add(sendButtonIconClass);
+    } else {
+      sendButton.classList.remove(sendButtonIconClass);
+      sendButton.classList.add(voiceButtonIconClass);
+    }
   }
   function sendMessageOnKeyDown(event) {
     if (event.keyCode === 13 && input.value) {
@@ -100,26 +142,14 @@ function createVkSnippetWidget() {
   }
   // -----
 
+  // Adds a widget with CTRL + SHIFT (any) to the page if it hasn`t showed up
+  document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.shiftKey) addVkWidget(e);
+  });
+  // Adds a widget to the page as the page is fully loaded and if VK messages are open (and if nothing is broken)
+  window.addEventListener("load", (e) => {
+    addVkWidget(e);
+  });
+
   return vkSnippetWidget;
 }
-
-function addVkWidget(e) {
-  let vkRightMenu = document.querySelector(
-    "#content > div > div.im-right-menu"
-  );
-  vkTextBox = document.querySelector("div[role='textbox']");
-  sendButton = document.querySelector(
-    "#content > div > div > div.im-page--history.page_block._im_page_history > div.im-page-history-w > div.im-page--chat-input._im_chat_input_w > div.im-chat-input.clear_fix._im_chat_input_parent > div.im-chat-input--textarea.fl_l._im_text_input._emoji_field_wrap._voice_field_wrap > div.im-chat-input--txt-wrap._im_text_wrap > button"
-  );
-
-  if (!vkRightMenu) return;
-  vkRightMenu.append(vkWidget);
-}
-
-document.addEventListener("keydown", (e) => {
-  if (e.ctrlKey && e.shiftKey) addVkWidget(e);
-});
-
-window.addEventListener("load", (e) => {
-  addVkWidget(e);
-});
