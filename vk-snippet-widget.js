62adc36ac5edeb71094da011f1fe66f5fb76e597
Anton Karpov <slam.caponezed@mail.ru>
2023-09-11 18:22:17 +0300

    Select node is added for extented variety. (And maybe something else, I dont remember)

--- vk-snippet-widget.js
+++ vk-snippet-widget.js
@@ -13,7 +13,40 @@ function createVkSnippetWidget() {
   let inputContainer = createNode({ tagType: "div", sendSnippet: false });
   let buttonContainer = createNode({ tagType: "div", sendSnippet: false });
 
+  // SELECT
+  let pagePrice = createNode({ tagType: "select", sendSnippet: false });
+  let pagePriceOptions = {
+    A4: 5,
+    "A4 Matte": 13,
+    "A4 Glossy": 15,
+    "A4 Glossy Thin": 13,
+    "A4 Glossy Sticker": 50,
+    "10x15 Glossy/Matte": 13,
+  };
+  let optionsKeys = Object.keys(pagePriceOptions);
+  for (let i = 0; i < optionsKeys.length; i++) {
+    let pagePriceOption = createNode({ tagType: "option", sendSnippet: false });
+    pagePriceOption.textContent = optionsKeys[i];
+    pagePriceOption.value = pagePriceOptions[optionsKeys[i]];
+    pagePrice.append(pagePriceOption);
+  }
+
+  // PAGE COUNT INPUT
+  let pageCountInput = createNode({ tagType: "input" });
+  pageCountInput.placeholder = "Количество страниц";
+  pageCountInput.addEventListener("keydown", sendMessageOnKeyDown);
+  pageCountInput.addEventListener("input", () => {
+    let pageCount = pageCountInput.value;
+    pageCount
+      ? (input.value = pageCount * pagePrice.value)
+      : (input.value = "");
+    handleInput();
+    if (!vkTextBox.innerHTML) pageCount.value = "";
+  });
+
+  // INPUT
   let input = createNode({ tagType: "input" });
+  input.addEventListener("input", handleInput);
   input.placeholder = "Стоимость печати (₽)";
 
   let moneyButton = createNode({
@@ -37,7 +70,7 @@ function createVkSnippetWidget() {
     tagType: "button",
     name: "🖨️",
     messageToSend:
-      "Печатаю 🖨️ (ˉ﹃ˉ) <br/> - цвет 🎨 <br/> - чб ⚫ <br/> - скрепки 📎 <br/> - скоросшиватели 🗂️ <br/> - файлики 📄) <br/> ",
+      "Печатаю 🖨️ (ˉ﹃ˉ) <br/> - цвет 🎨 <br/> - чб ⚫ <br/> - фото 🖼️(матовое, глянцевое) <br/> - скрепки 📎 <br/> - скоросшиватели 🗂️ <br/> - файлики 📄 <br/> ",
   });
   let doneButton = createNode({
     tagType: "button",
@@ -49,6 +82,8 @@ function createVkSnippetWidget() {
   vkSnippetWidget.append(inputContainer);
   vkSnippetWidget.append(buttonContainer);
 
+  inputContainer.append(pageCountInput);
+  inputContainer.append(pagePrice);
   inputContainer.append(input);
   inputContainer.append(moneyButton);
 
@@ -73,13 +108,11 @@ function createVkSnippetWidget() {
       switch (true) {
         case node.tagName == "INPUT": {
           node.type = "number";
-
-          node.addEventListener("input", handleInput);
           node.addEventListener("keydown", sendMessageOnKeyDown);
           break;
         }
         case node.tagName == "BUTTON": {
-          node.addEventListener("click", (event) => {
+          node.addEventListener("click", () => {
             vkTextBox.click();
             vkTextBox.innerHTML = messageToSend;
             if (sendSnippet) sendButton.click();
@@ -90,6 +123,7 @@ function createVkSnippetWidget() {
               setTimeout(() => (vkTextBox.textContent = ""), 0);
             }
             input.value = "";
+            pageCountInput.value = "";
           });
           break;
         }
@@ -100,7 +134,7 @@ function createVkSnippetWidget() {
       return err;
     }
   }
-  function addVkWidget(e) {
+  function addVkWidget() {
     try {
       let vkRightMenu = document.querySelector(
         "#content > div > div.im-right-menu > div.page_block.ui_rmenu._im_right_menu.ui_rmenu_pr"
@@ -110,7 +144,10 @@ function createVkSnippetWidget() {
         "#content > div > div > div.im-page--history.page_block._im_page_history > div.im-page-history-w > div.im-page--chat-input._im_chat_input_w > div.im-chat-input.clear_fix._im_chat_input_parent > div.im-chat-input--textarea.fl_l._im_text_input._emoji_field_wrap._voice_field_wrap > div.im-chat-input--txt-wrap._im_text_wrap > button"
       );
       sendButton.addEventListener("click", () => {
-        if (input.value) input.value = "";
+        if (input.value) {
+          input.value = "";
+          pageCountInput.value = "";
+        }
       });
       if (!vkRightMenu) return;
       vkRightMenu.append(vkWidget);
@@ -120,11 +157,12 @@ function createVkSnippetWidget() {
   }
 
   // Event handlers
-  function handleInput(event) {
-    let targetValue = event.target.value;
+  function handleInput() {
+    let targetValue = input.value;
     vkTextBox.click();
-    vkTextBox.innerHTML = `${targetValue}₽ на 89201006977 ( Тинькофф, сбербанк ) или наличными.`;
-
+    vkTextBox.innerHTML = `${targetValue}₽ за ${pageCountInput.value} ед. ${
+      optionsKeys[pagePrice.selectedIndex]
+    } на 89201006977 ( Тинькофф, сбербанк ) или наличными.`;
     if (!targetValue) vkTextBox.textContent = "";
     if (vkTextBox.innerHTML) {
       sendButton.classList.remove(voiceButtonIconClass);
@@ -135,21 +173,18 @@ function createVkSnippetWidget() {
     }
   }
   function sendMessageOnKeyDown(event) {
-    if (event.keyCode === 13 && input.value) {
+    if (event.keyCode === 13 && event.target.value) {
       sendButton.click();
-      input.value = "";
+      event.target.value = "";
     }
   }
-  // -----
 
   // Adds a widget with CTRL + SHIFT (any) to the page if it hasn`t showed up
   document.addEventListener("keydown", (e) => {
     if (e.ctrlKey && e.shiftKey) addVkWidget(e);
   });
   // Adds a widget to the page as the page is fully loaded and if VK messages are open (and if nothing is broken)
-  window.addEventListener("load", (e) => {
-    addVkWidget(e);
-  });
+  window.addEventListener("load", (e) => addVkWidget(e));
 
   return vkSnippetWidget;
 }
